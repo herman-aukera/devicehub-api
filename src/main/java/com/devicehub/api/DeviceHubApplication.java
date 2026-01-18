5ebfa8512772c876009725016f72d4e03dbeb0df
GG <gg@ai4devs.com>
2026-01-18 16:21:13 +0100

    chore(init): project skeleton with Maven, Docker, gitignore and logging

---
+++ src/main/java/com/devicehub/api/DeviceHubApplication.java
@@ -0,0 +1,19 @@
+package com.devicehub.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+
+@SpringBootApplication
+@Slf4j
+public class DeviceHubApplication {
+
+    public static void main(String[] args) {
+        log.info("DeviceHub API starting with Java {} and Spring Boot {}", 
+                System.getProperty("java.version"), 
+                SpringVersion.getVersion());
+        SpringApplication.run(DeviceHubApplication.class, args);
+        log.info("DeviceHub API started successfully");
+    }
+}
